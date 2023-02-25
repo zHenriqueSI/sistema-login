de87d87a82aef6be3627d7f10cad517ac8bfb9bf
henriquegomesnunes <henriquegomesnunes19@usp.br>
2023-02-25 11:12:24 -0300

    feat: new table - users_tokens

--- files/models.py
+++ files/models.py
@@ -1,6 +1,7 @@
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from connection import Base, engine
+import datetime
 
 class Users(Base):
     __tablename__ = "users"
@@ -9,6 +10,12 @@ class Users(Base):
     email = Column(String(200))
     senha = Column(String(32))
 
+class Tokens(Base):
+    __tablename__ = "users_tokens"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    token = Column(String(100))
+    dt_valid = Column(DateTime, default=datetime.datetime.utcnow())
 
 if __name__ == '__main__':
     Base.metadata.create_all(bind=engine)
